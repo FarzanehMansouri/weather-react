91e4530b33714243ffb3cc113696528b881618ec
FarzanehMansouri <96907470+FarzanehMansouri@users.noreply.github.com>
2022-03-24 07:06:52 +0330

    add github src address

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <h1>React Weather App</h1>
       <Search />
+      <h5><a href="https://github.com/FarzanehMansouri/weather-react" target="_blank" rel="noreferrer">Github Source</a></h5>
     </div>
   );
 }
